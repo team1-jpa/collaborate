bfbbdd510b59fa1da6bd7883d3e3703a07d38447
kenykim <gustmd9146@gmail.com>
2024-10-11 15:14:01 +0900

    셀렉트바이 코드 완성.

--- JPAStudy/src/main/java/com/ohgiraffers/springdatajpa/menu/repository/MenuRepository.java
+++ JPAStudy/src/main/java/com/ohgiraffers/springdatajpa/menu/repository/MenuRepository.java
@@ -1,9 +1,12 @@
 package com.ohgiraffers.springdatajpa.menu.repository;
 
 
+import com.ohgiraffers.springdatajpa.menu.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
-public interface MenuRepository  {
+public interface MenuRepository extends JpaRepository<Menu, Integer> {
 
 
 }
--- JPAStudy/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
+++ JPAStudy/src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
@@ -1,12 +1,19 @@
 package com.ohgiraffers.springdatajpa.menu.service;
 
-
 import com.ohgiraffers.springdatajpa.menu.dto.MenuDTO;
 import com.ohgiraffers.springdatajpa.menu.entity.Menu;
+import com.ohgiraffers.springdatajpa.category.repository.CategoryRepository;
 import com.ohgiraffers.springdatajpa.menu.repository.MenuRepository;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
