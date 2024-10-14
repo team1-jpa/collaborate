c2418a8e204f88608419da0d3b0500205e97cf14
kenykim <gustmd9146@gmail.com>
2024-10-14 13:57:21 +0900

    셀렉트바이 단위테스트 작성 완료.

--- JPAStudy/src/test/java/com/ohgiraffers/springdatajpa/Chap06SpringDataJpaApplicationTests.java
+++ JPAStudy/src/test/java/com/ohgiraffers/springdatajpa/Chap06SpringDataJpaApplicationTests.java
@@ -1,13 +1,49 @@
 package com.ohgiraffers.springdatajpa;
 
+import com.ohgiraffers.springdatajpa.menu.dto.MenuDTO;
+import com.ohgiraffers.springdatajpa.menu.service.MenuService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Chap06SpringDataJpaApplicationTests {
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class Chap06SpringDataJpaApplicationTests {
+
+    @Autowired
+    MenuService menuService;
+
+    //코드 검색으로 메뉴 찾기 테스트
     @Test
-    void contextLoads() {
+    public void foundCodeMenu() {
+
+        //given
+        int menuCode = 1;
+
+        //when
+        MenuDTO findMenu = menuService.findMenuByCode(menuCode);
+
+        //then
+        assertNotNull(findMenu);
+        System.out.println("findMenu = " + findMenu);
+    }
+
+    //가격 검색으로 메뉴 리스트 호출하기 테스트
+    @Test
+    public void findMenuPrice() {
+
+        //given
+        int price = 10000;
+
+        //when
+        List<MenuDTO> menuPriceList = menuService.findByMenuPrice(price);
+
+        //then
+        assertNotNull(menuPriceList);
+        System.out.println("menuPriceList = " + menuPriceList);
     }
 
 }
